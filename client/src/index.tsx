27640923b3bbc560cb6edf68088aab689bd2eb0c
Raymond Feng <rf.raymondfeng@gmail.com>
2021-11-30 16:29:12 +1300

    Initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainRouter from './pages/MainRouter';
+
+import 'normalize.css';
+import './styles/style.scss';
+
+ReactDOM.render(<MainRouter />, document.getElementById('root'));
